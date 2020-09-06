52c0de4fc80cd0e0050f4efbbd4bd6bd7837e550
aandy300 <aandy300@gmail.com>
2020-09-07 01:18:24 +0800

    fix-20200907

--- del_txt.py
+++ del_txt.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 # 確認檔案是否在 > 去除不要的字元 > 轉繁體 > 另存
-=======
-# 去除不要字元 / 轉繁體 / 另存
-import time
-import random
-# import progressbar #進度條 測試用
-from zhconv import convert #簡轉繁
->>>>>>> 81a450842a61196139215da657e34e1085ecf335
 
 # 檔案是否存在 中文解說
 # https://blog.gtwang.org/programming/python-howto-check-whether-file-folder-exists/
@@ -16,22 +8,11 @@ import progressbar # 進度條 測試用
 from zhconv import convert # 簡轉繁
 
 x = 0
-<<<<<<< HEAD
 # 進度條 測試用(方便瞭解進度)
 bar = progressbar.ProgressBar(max_value=1000000) 
 
 # 開啟要編輯的檔案 C:/Users/admin/Desktop/1.txt 檔名 1.txt
 with open ('C:/Users/admin/Desktop/1.txt', 'r', encoding='UTF-8') as f :
-=======
-# bar = progressbar.ProgressBar(max_value=1000000) #進度條 測試用
-
-with open ('C:/Users/admin/Desktop/1.txt', 'r', encoding='UTF-8') as f :
-    print(f)
-    for line in f :
-        text = ('黃色')
-        line = line.replace(" ", "") #去除空格
-        line = convert(line, 'zh-tw')
->>>>>>> 81a450842a61196139215da657e34e1085ecf335
 
     # 確認檔案是否存在 存在的話刪除 防止追加寫入
     if os.path.isfile('C:/Users/admin/Desktop/z.txt'):
