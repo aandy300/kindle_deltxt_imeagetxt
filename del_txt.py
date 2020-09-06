068bb0b40839a77b6759571ffa74e08ccac77636
aandy300 <aandy300@gmail.com>
2020-09-07 01:16:01 +0800

    fix-20200907

--- del_txt.py
+++ del_txt.py
@@ -1,29 +1,42 @@
-# 去除不要字元 / 轉繁體 / 另存
-import time
-import random
-import progressbar #進度條 測試用
-from zhconv import convert #簡轉繁
+# 確認檔案是否在 > 去除不要的字元 > 轉繁體 > 另存
 
-datas = []
-cleartext = []
+# 檔案是否存在 中文解說
+# https://blog.gtwang.org/programming/python-howto-check-whether-file-folder-exists/
+
+import os, time, random
+import progressbar # 進度條 測試用
+from zhconv import convert # 簡轉繁
 
 x = 0
-bar = progressbar.ProgressBar(max_value=1000000) #進度條 測試用
+# 進度條 測試用(方便瞭解進度)
+bar = progressbar.ProgressBar(max_value=1000000) 
 
-with open ('C:/Users/admin/Desktop/z.txt', 'r', encoding='UTF-8') as f :
-    print(f)
+# 開啟要編輯的檔案 C:/Users/admin/Desktop/1.txt 檔名 1.txt
+with open ('C:/Users/admin/Desktop/1.txt', 'r', encoding='UTF-8') as f :
+
+    # 確認檔案是否存在 存在的話刪除 防止追加寫入
+    if os.path.isfile('C:/Users/admin/Desktop/z.txt'):
+        print("檔案存在。")
+        os.remove('C:/Users/admin/Desktop/z.txt')
+    else:
+        print("檔案不存在。")
+
+    # 去除指定文字 將指定文字以外 存入 z.txt
     for line in f :
-        text = ('不要的行 裡面的字')
-        line = line.replace(" ", "") #去除空格
-        line = convert(line, 'zh-tw')
-
-        print(line)
-
+        # 通過輸入的字 來刪除文本不要的行
+        text = ('您在位置')
+        # 去除空格
+        line = line.replace(" ", "") 
+        
+        # 去除的行 去1 + x+1
         if text in line:
-            x +=1
-            print(x)
+            x += 1
+        # 沒有被去除的存入 z.txt
         else:
-            with open ('C:/Users/admin/Desktop/z.txt', 'a', encoding='UTF-8') as f : #另存為z.txt
+            # 簡體字轉繁體字
+            line = convert(line, 'zh-tw')
+            # "寫入"z.txt  非另存為z.txt
+            with open ('C:/Users/admin/Desktop/z2.txt', 'a', encoding='UTF-8') as f : 
                 f.write(line)
-                print('------------------------', line, '分隔線分隔線分隔線分隔線分隔線分隔線分隔線')
                 f.close()
+    print('去除的行數:', x)
