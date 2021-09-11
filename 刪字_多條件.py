b8581b9471aeee323e08b0a4ceb9cadb92b05743
aandy300 <aandy300@gmail.com>
2021-09-11 11:12:16 +0800

    20210911 改一下備註

--- 刪字_多條件.py
+++ 刪字_多條件.py
@@ -1,30 +1,42 @@
-import os, time, random
-from zhconv import convert # 簡轉繁
-
+import os, time, random, re
+# 簡轉繁
+from zhconv import convert 
+# 開啟文件、存檔ui
 from tkinter.filedialog import askopenfilename,asksaveasfilename
 from tkinter import Tk
-import re
+
 
 # 結論記憶體出問題?
-
 # 結論 >  用 IF[01234]..... 
 
-del_text = "樊登", "您在" # 要刪除的行 包含的文字
+# 要刪除的行 包含的文字
+del_text = "樊登", "您在" 
+# 暫存取用
 text_array = []
 ok_text = []
+# tkinter 開始儲存ui
 filename = askopenfilename(filetypes=[("Image files", "*.txt"), ("All Files", "*.*")]) # 檔案類型 篩選
-
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing # 隱藏tk ui
-savefilename = 'C:/Users/admin/Desktop/新增資料夾/01.txt' # 要存檔的名稱與路徑
-
+savefilename= asksaveasfilename(filetypes=[("Image files", "*.txt"), ("All Files", "*.*")]) 
+# we don't want a full GUI, so keep the root window from appearing # 隱藏tk ui
+Tk().withdraw() 
+# count用
 x = 0
 
+# 20210722 存檔修改 # savefilename = 'C:/Users/admin/Desktop/00/01.txt' # 要存檔的名稱與路徑
+# 20210911 改一下備註
+
+# 開啟檔案叫f
 with open (filename, 'r', encoding='UTF-8') as f :
 
+    # type(object_or_name, bases, dict) type(object) -> 印出對象的類型 
+    # type(name, bases, dict) -> 製作一個新類型
     type(del_text)
 
+    # 一行一行印出
     for line in f:
+        # replace去空白
         line = line.replace(" ", "")
+        # 把要刪除的字一個一個跑過一變 有的話挑出並計數 > 沒有的加入到 ok_text.append(line)
         for del_ary in del_text:
             if del_text[0] in line:
                 x +=1
@@ -35,10 +47,9 @@ with open (filename, 'r', encoding='UTF-8') as f :
             else:
                 ok_text.append(line)
                 
-
-
+    # 跑過濾掉已經過濾掉刪除字句子 > 開啟文件(準備寫入用) > 轉繁體字 > 寫入並加入換行符號 > 關閉
     for line in ok_text:
-        with open (savefilename, 'a', encoding='UTF-8') as f : 
+        with open (savefilename + '.txt', 'a', encoding='UTF-8') as f : 
             line = convert(line, 'zh-tw')
             f.write(line+'\n')
             f.close()
